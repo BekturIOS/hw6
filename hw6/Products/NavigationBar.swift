f6710e9f108869b3616876ab7d441ca7995029ff
BekturIOS <126708400+BekturIOS@users.noreply.github.com>
2023-04-20 01:14:46 +0600

    Update NavigationBar.swift

--- hw6/Products/NavigationBar.swift
+++ hw6/Products/NavigationBar.swift
@@ -10,32 +10,35 @@ import SwiftUI
 
 struct NavigationBar: View {
     var body: some View {
-        HStack{
-            CustomButton(image: Image("menu"))
+       
             HStack{
-                Spacer()
-                VStack{
-                    CustomText(
-                        text: "Delivery to",
-                        color: Color(.gray),
-                        font: .thin                      )
-                    CustomText(
-                        text: "Manas Ave",
-                        color: Color(.black),
-                        font: .bold)
-                }
-    
-                CustomButton(image: Image("vector"))
-                Spacer()
-                CustomButton(
-                    image: Image("basket"),
-                    color: Color(.black)
+                CustomButton(image: Image("menu"))
+                HStack{
+                    Spacer()
+                    VStack{
+                        CustomText(
+                            text: "Delivery to",
+                            color: Color(.gray),
+                            font: .thin                      )
+                        CustomText(
+                            text: "Manas Ave",
+                            color: Color(.black),
+                            font: .bold)
+                    }
                     
-                )
+                    CustomButton(image: Image("vector"))
+                    Spacer()
+                   
+                    NavigationLink(destination: SecondView(),
+                                   label:  { Image("basket")
+                            .frame(width: 50, height: 50)
+                                   
+                    })
+                    .cornerRadius(15)
                 
-                .cornerRadius(15)
+                    .frame(width: 30, height: 30)
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
         }
     }
 }
